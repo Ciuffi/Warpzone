d0ab5ce2b119a4705a698debf6f0021826edcadb
Unknown <ciuffi9@mac.com>
2018-06-08 16:14:25 -0700

    Add moving floors

---
+++ Assets/Scripts/FloorHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorHandler : MonoBehaviour {
+
+	private Vector2 _reset = new Vector2(28.749f, 0.017f);
+	Vector2 _endPosition = new Vector2(-29, 0.017f);
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 trans = Vector2.left * Time.deltaTime * FindObjectOfType<SpawnHandler>().Speed;
+		transform.Translate(trans);
+		if (transform.position.x < _endPosition.x) transform.position = _reset;
+	}
+}
